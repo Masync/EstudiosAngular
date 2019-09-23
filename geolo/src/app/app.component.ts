be5299fec99ba314a41178e1ffd91d73bcbeaccd
Juan Manuel Patiño <jumapa662@gmail.com>
2019-09-23 11:27:30 -0500

    Utilizar el servicio de google maps
    se tiene el error de la api key, pero la interfaz esta completa

--- geolo/src/app/app.component.ts
+++ geolo/src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
-
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -10,6 +19,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  map: GoogleMap;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +32,43 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
     });
   }
 }
--- geolo/src/app/home/home.page.ts
+++ geolo/src/app/home/home.page.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +16,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  map: GoogleMap;
+  @ViewChild('mapElement', {static: false}) home: HomePage;
+  // tslint:disable-next-line: variable-name
+  private _mapElement: {
+    nativeElement: any;
+  };
+  public get mapElement():{
+    nativeElement: any;
+  } {
+    return this._mapElement;
+  }
+  public set mapElement(value:{
+    nativeElement: any;
+  }){
+    this._mapElement = value;
+  }
   constructor() {}
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      API_KEY_FOR_BROWSER_RELEASE: '(AIzaSyC40j6ItrFinhgURPaOd-fEAzluFVYwUUo)',
+      API_KEY_FOR_BROWSER_DEBUG: '(AIzaSyC40j6ItrFinhgURPaOd-fEAzluFVYwUUo)'
+    });
+
+    // tslint:disable-next-line: prefer-const
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    // tslint:disable-next-line: prefer-const
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 
 }
